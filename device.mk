585ed664c764e4e71ca732153ba079a49c8d5aee
bigbiff <bigbiff@teamw.in>
2022-02-22 00:20:17 +0000

    embedded: call embedded.mk to remove goldfish dependencies

--- device.mk
+++ device.mk
@@ -108,6 +108,6 @@ PRODUCT_BOOT_JARS += oem-services # QCOM services
 endif
 
 
-$(call inherit-product, build/target/product/full.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
